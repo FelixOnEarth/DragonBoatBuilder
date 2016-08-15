ffa2af3e3e23e774b19ca05c45b56378d96d96d4
felix <felix@DESKTOP-15T09KC>
2016-08-15 10:41:10 +0200

    Variable name changed

--- DragonboatBuilder/src/com/dragonboatbuilder/persons/Racer.java
+++ DragonboatBuilder/src/com/dragonboatbuilder/persons/Racer.java
@@ -4,67 +4,67 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class Racer{
-    private String Forename;
-    private String Surname;
-    private String Nickname;
-    private String Email;
-    private String Position;
-    private Double Weight;
-    private GregorianCalendar Birth;
+    private String forename;
+    private String surname;
+    private String nickname;
+    private String email;
+    private String position;
+    private Double weight;
+    private GregorianCalendar birth;
 
     public Racer() {
     }
 
     public String getForename() {
-        return Forename;
+        return forename;
     }
 
     public void setForename(String Forename) {
-        this.Forename = Forename;
+        this.forename = Forename;
     }
 
     public String getSurname() {
-        return Surname;
+        return surname;
     }
 
     public void setSurname(String Surname) {
-        this.Surname = Surname;
+        this.surname = Surname;
     }
 
     public String getNickname() {
-        return Nickname;
+        return nickname;
     }
 
     public void setNickname(String Nickname) {
-        this.Nickname = Nickname;
+        this.nickname = Nickname;
     }
 
     public String getEmail() {
-        return Email;
+        return email;
     }
 
     public void setEmail(String Email) {
-        this.Email = Email;
+        this.email = Email;
     }
 
     public String getPosition() {
-        return Position;
+        return position;
     }
 
     public void setPosition(String Position) {
-        this.Position = Position;
+        this.position = Position;
     }
 
     public Double getWeight() {
-        return Weight;
+        return weight;
     }
 
     public void setWeight(Double Weight) {
-        this.Weight = Weight;
+        this.weight = Weight;
     }
 
     public GregorianCalendar getBirth() {
-        return Birth;
+        return birth;
     }
 
     public void setBirth(String Birth) {
@@ -78,7 +78,7 @@ public class Racer{
         GregorianCalendar calendar = new GregorianCalendar(i2, i1, i0); //Year,Month,Day of Month
         
         System.out.println(calendar);
-        this.Birth = calendar;
+        this.birth = calendar;
     }
     
     
